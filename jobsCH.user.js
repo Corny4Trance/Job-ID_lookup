57aa8dfb1d13e7ab147bb3fb34441ccc335b408b
Corny4Trance <Corny4Trance@users.noreply.github.com>
2020-11-08 07:18:15 +0100

    Adption to structural changes on jobs.ch page

--- jobsCH.user.js
+++ jobsCH.user.js
@@ -1,54 +1,71 @@
 // ==UserScript==
 // @name     jobs.ch
 // @description  Get the Job-ID and display it
-// @version  1
+// @version  1 2018-07-07
 // @match    https://www.jobs.ch/de/firmen/3758-kantonsspital-st-gallen/stellenangebote/*
 // @require  https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
 // @grant    GM_addStyle
 // ==/UserScript==
-
+	
 /*- The @grant directive is needed to work around a major design
     change introduced in GM 1.0.
-    It restores the sandbox.
-*/
-/*
-Tweak to your needs:
-replace the @match directive with your company
-replace the regex .*career_job_req_id=(\d+).* to what ever is needed to find your Job-ID in the apply URL
+    It restores the sandbox. */
 
-Usage Hint
-jobs.ch paginates the listing. If you want copy the list you need to work thru all result pages
-The script needs a hard reload of the listing page to trigger the lookup process. Comming from the company page the script will therefore not run.
-*/
+// a part of the page dom is loaded twice. Once when received from jobs.ch and once after google tag manager and Krux (formerly jobs.ch was using hotjar) have responded.
+// wait for this have finished
+// waitForKeyElements('#_hjRemoteVarsFrame', actionFunction); //hotjar
+waitForKeyElements('#kx-proxy-st5kwdon6', actionFunction); // Krux cdn.krxd.net
 
+function actionFunction(jNode){
+   // remove superfluous information 
+  $('.matching-indicator').remove();
+  $('.media-left').remove();
+  $('.media-right').remove();
+  $('.serp-item-head-2').remove();
+  $('.serp-item-head-3').remove();
+  
+  // condens formatting
+  $('.serp-item-content-inner').css("padding", "0px 5px");
+  $('.serp-item-head-1').css("font-size", "1em").css("margin", "0");
+  $('.serp-item-meta').css("padding", "0").css("margin", "0");
+  $('.serp-item-job-info').css("min-height", "auto");
+  
+  
+  // if there are own postings on the page
+  if($('.serp-item:not(:has(.serp-item-info))').length){
+    // add container table
+    $('.box-head').css("padding", "0").html('<table id="MECtbl"><tr style="background-color: #a4d600">' +
+                                                '<th>Job-ID</th><th>Titel</th><th>jobs.ch Post-ID</th><th>Datum</th>' +
+                                                '</tr></table>'); 
 
-waitForKeyElements ('div.serp-item:has(div.matching-indicator)', actionFunction);
+    // skip postings found on other platforms
+    $('.serp-item:not(:has(.serp-item-info))').css("background-color", "#a4d600").each(function(){
+      var title = $('h2 a', this).text();
+      var postDatum = $('.badge:first span', this).text();
+      var postHREF = $('h2 a', this).attr('href');
+      postHREF = (postHREF.split("?"))[0];
+      var postID = postHREF.replace(/.*detail\/(\d*)\/.*/, '$1');
+      $('#MECtbl').append('<tr><td id="MECjobID' + postID + '"></td><td>' + title + '</td><td><a href="' + postHREF + '">' + postID + '</a></td><td>' + postDatum +'</td></tr>');
+      this.remove(); // the original entry is removed after having been duplicated to the container table
+      var xmlhttp = new XMLHttpRequest(); 
+      xmlhttp.onreadystatechange = function(){
+       if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
+          var dom = $(xmlhttp.responseText);
+          var jobID = dom.find('a.t--external-apply').attr('href').replace(/.*career_job_req_id=(\d+).*/, "$1");
+          $('#MECjobID' + postID).html('<a href="https://career012.successfactors.eu/sfcareer/jobreqcareer?company=KSSG&jobId=' + jobID + '">' + jobID + '</a>');
+        }
+      }
+      xmlhttp.open("GET", postHREF, false);
+      //console.log(postID + " aufgerufen mit " + postHREF);
+      xmlhttp.send();
+    })
+	}
 
-function actionFunction (jNode) {
-  if($('div.box-head h2').length) 
-    $('div.box-head').html('<table id="MECtbl"><tr>' +
-                '<th style="border-right: 1px #e5e5e6 solid; padding: 5px;">Job-ID </th>' +
-                '<th style="border-right: 1px #e5e5e6 solid; padding: 5px;">Titel</th>' +
-                '<th style="border-right: 1px #e5e5e6 solid; padding: 5px;">JobsCH Post-ID</th>' +
-                '<th style="border-right: 1px #e5e5e6 solid; padding: 5px;">Datum</th>' +
-                '</tr></table>');
-  var postID = jNode.attr("class").replace(/.*x--serp-item-(\d*) .*/, '$1');
-  if($('#MEC_JobID' + postID).length == 0)
-    $('#MECtbl tbody').append('<tr>' +
-                '<td id="MEC_JobID' + postID + '" style="border-right: 1px #e5e5e6 solid; padding: 5px;"> ... <div id="MECtmp' + postID + '"></div></td>' +
-                '<td style="border-right: 1px #e5e5e6 solid; padding: 5px;">' + jNode.find('h2').text() + '</td>' +
-                '<td style="border-right: 1px #e5e5e6 solid; padding: 5px;"><a href="https://www.jobs.ch/de/stellenangebote/detail/' + postID + '" target="_blank">' + postID + '</td>' +
-                '<td style="border-right: 1px #e5e5e6 solid; padding: 5px;">' + jNode.find('div.badge-pool span:first').text() + '</td>' +
-                '</tr>');
-  $('#MECtmp' + postID).load('https://www.jobs.ch/de/stellenangebote/detail/' + postID +' a.t--external-apply',
-    function(response, status, xhr){
-      var jobID = $('a', this).attr('href').replace(/.*career_job_req_id=(\d*).*/, '$1');
-      var jobHREF= 'https://career012.successfactors.eu/sfcareer/jobreqcareer?company=KSSG&jobId=' + jobID;
-      //console.log(postID + '\t' + jobID + '\t' + status);
-      $('#MEC_JobID' + postID).html('<a href="' + jobHREF +'" target="_blank">' + jobID +'</a>');
-    }
-  );
-  jNode.find('div.serp-item-content').remove();
-  jNode.find('div.matching-indicator').remove();
-}
+  // prevent events on the pagination links
+  $('ul.pagination a').each(function(){
+    //duplicate original link and remove it afterwards
+    $(this).after('<a href="' + $(this).attr("href") + '">' + $(this).html() + '</a>');
+    this.remove();
+  });
+} 
