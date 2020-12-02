2b415047941fc3dc33fd261eaa612fde8611bbd1
Corny4Trance <Corny4Trance@users.noreply.github.com>
2020-12-02 16:47:04 +0100

    Update ostjobCH.user.js
    Ostjob is again delivering the whole job list on initial response. Modified structure accordingly

--- ostjobCH.user.js
+++ ostjobCH.user.js
@@ -1,61 +1,48 @@
 // ==UserScript==
 // @name     ostjob.ch
 // @description  Get the Job-ID and display it
-// @version  3 2019-06-08
+// @version  3 2020.12.02
 // @author marcel.eckstein@kssg.ch
 // @match    https://www.ostjob.ch/firma/kantonsspital-st-gallen/3613*
 // @match    https://neu.ostjob.ch/firma/kantonsspital-st-gallen/3613*
-// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
-// @grant    GM_addStyle
+// @require  https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
 // ==/UserScript==
-	
-/*- The @grant directive is needed to work around a major design
-    change introduced in GM 1.0.
-    It restores the sandbox.
-*/
 
-// anchors for different ostjob versions
-var versions = {
-	"v2018" : {"container" : 'div.company-details__company-info', 
-             "keyElement" : 'li.vacancy-list__vacancy-small-item',
-             "postDate" : 'div.vacancy-small-item__date',
-             "postingAd" : 'div.vacancy-layout__action-links a.action-links__icon-email'
-            },
-	"v2019" : {"container" : 'div.company-details-card__container', 
-             "keyElement" : 'div.vacancy-list-card__body',  
-             "postDate" : 'li.vacancy-list-card__date',
-             "postingAd" : 'a.button.button--primary.button--block'
-            }
-};
-
-// check Ostjob platform release
-var version = "unknown";
-if($(versions.v2018.container).length) version = versions.v2018;
-if($(versions.v2019.container).length) version = versions.v2019;
-if(version === "unknown") alert("Kann Ostjob Version nicht erkennen !");
+// clear structure to avoid reloading of the page after auth script
+$('div.stats').remove();
+$('#root > div').unwrap();
+$('#root').unwrap();
+$('body > div').unwrap();
+// social media is not needed here
+$('div.sticky-placeholder').remove(); 
+// location structure is not needed here
+$('div.company-vacancy-list').append($('a.vacancy-list-card__link-header'));
+$('div.company-vacancy-group').remove();
 
 // inject table for job ad list
-var MECtbl = $(version.container).html('<table><tr style="background-color: #a4d600">' +
-                                            '<th>Job-ID</th><th>Titel</th><th>Ostjob Post-ID</th><th>Datum</th>' +
-                                            '</tr></table>');
-
-waitForKeyElements (version.keyElement, actionFunction);
-
-function actionFunction(jNode){
-  var title = $('h2 a', jNode).text();
-  var postHREF = $('h2 a', jNode).attr('href');
+$('div.company-details-card__container').replaceWith('<table id="MECtbl"><tr style="background-color: #a4d600">' +
+  '<th>Job-ID</th><th>Titel</th><th>Ostjob Post-ID</th><th>Datum</th>' +
+  '</tr></table>');
+// iterate list and move to table
+$('a.vacancy-list-card__link-header').each(function(){
+  var title = $('.vacancy-list-card__title', this).text();
+  var postHREF = $(this).attr('href');
   var postID = postHREF.replace(/.*\/(\d*)/, '$1');
-  var postDatum = $(version.postDate, jNode).text();
-	MECtbl.find('tbody').append('<tr><td id="MECjobID' + postID + '"></td><td>' + title + '</td><td><a href="' + postHREF + '">' + postID + '</a></td><td>' + postDatum +'</td></tr>');
+  var postDatum = $('.vacancy-list-card__date', this).text();
+	$('#MECtbl > tbody').append('<tr><td id="MECjobID' + postID + '">...</td><td>' + title + '</td><td><a href="' + postHREF + '">' + postID + '</a></td><td>' + postDatum +'</td></tr>');
+  $(this).remove();
   var xmlhttp = new XMLHttpRequest(); 
   xmlhttp.onreadystatechange = function(){
+      console.log('xmlhttp.readyState ' + xmlhttp.readyState);
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200){
       var dom = $(xmlhttp.responseText);
-      var jobID = dom.find(version.postingAd).attr('href').replace(/.*career_job_req_id=(\d+).*/, "$1");
+      console.log(xmlhttp.responseText);
+      var jobID = dom.find('a.button.button--primary.button--block').attr('href').replace(/.*career_job_req_id=(\d+).*/, "$1");
       $('#MECjobID' + postID).html('<a href="https://career012.successfactors.eu/sfcareer/jobreqcareer?company=KSSG&jobId=' + jobID + '">' + jobID + '</a>');
-    }
-  }
+    };
+  }; 
   xmlhttp.open("GET", postHREF, false);
   xmlhttp.send();
-} 
+});
+//remove original list
+$('div.company-vacancy-list').remove();
